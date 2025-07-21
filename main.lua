cb00693a97b9976dbf2822a3edae436320f15d1d
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-07-21 20:52:39 +0100

    Test inline tags

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local fs = require("fs")
 local tokenise = require("./html/tokeniser")
 local parse = require("./html/parser")
 
-local src = fs.readFileSync("./src/index.html")
+local src = fs.readFileSync("./test.html")
 
 local t = tokenise(src)
 
@@ -37,5 +37,5 @@ print()
 print()
 
 local serialise = require("./html/serialiser")
---fs.writeFileSync("./src/out.html", serialise(parsed)) -- obviously just a test
+fs.writeFileSync("./out.html", serialise(parsed)) -- obviously just a test
 -- in the real tool, we wont EVER be writing to src, only reading
