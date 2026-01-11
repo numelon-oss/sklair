f9013bfee97a8659ac33e6ffe9ebc99304dc9409
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-01-11 16:41:03 +0000

    Add debounce to devserver and fix watching regresison
    - Sklair's build state has been excluded during filesystem watching in the devserver to prevent self-triggered rebuild loops, in the event of the input directory and the config directory being the same (since .sklair is created in the config directory)
    - A debounce of 150 milliseconds has been added to filesystem watching so that we do not get 500 re-compilations within a second because of one change in a code editor/IDE
    - all of this fixed a regression introduced by sandboxed hooks when input = ./ (ie same as config file dir, where .sklair/* is written)

--- devserver/watch.go
+++ devserver/watch.go
@@ -4,9 +4,67 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+func debounce(in <-chan bool, delay time.Duration) <-chan bool {
+	out := make(chan bool)
+
+	go func() {
+		defer close(out)
+
+		var (
+			timer   *time.Timer
+			pending bool
+		)
+
+		for {
+			var timerC <-chan time.Time
+			if timer != nil {
+				timerC = timer.C
+			}
+
+			select {
+			case _, ok := <-in:
+				if !ok {
+					return
+				}
+
+				pending = true
+
+				if timer == nil {
+					timer = time.NewTimer(delay)
+				} else {
+					if !timer.Stop() {
+						<-timer.C
+					}
+					timer.Reset(delay)
+				}
+
+			case <-timerC:
+				if pending {
+					out <- true
+					pending = false
+				}
+				timer = nil
+			}
+		}
+	}()
+
+	return out
+}
+
+func shouldWatch(path string) bool {
+	base := filepath.Base(path)
+	switch base {
+	case ".sklair", ".git":
+		return false
+	}
+
+	return true
+}
 
 // track changes from the following directories:
 // - source directory (excluding components dir, if it is within the source directory)
@@ -37,7 +95,11 @@ func Watch(dir string) (<-chan bool, <-chan error) {
 				return err
 			}
 			if d.IsDir() {
-				return watcher.Add(path)
+				if shouldWatch(path) {
+					return watcher.Add(path)
+				} else {
+					return filepath.SkipDir
+				}
 			}
 
 			return nil
@@ -55,7 +117,7 @@ func Watch(dir string) (<-chan bool, <-chan error) {
 
 				// handle new directories dynamically
 				if e.Op&fsnotify.Create != 0 {
-					if info, err := os.Stat(e.Name); err == nil && info.IsDir() {
+					if info, err := os.Stat(e.Name); err == nil && info.IsDir() && shouldWatch(e.Name) {
 						_ = watcher.Add(e.Name)
 					}
 				}
@@ -74,5 +136,5 @@ func Watch(dir string) (<-chan bool, <-chan error) {
 		}
 	}()
 
-	return events, errs
+	return debounce(events, 150*time.Millisecond), errs
 }
