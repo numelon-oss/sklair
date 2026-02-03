c6c073691689852a6c84868c5099927b7348131a
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-02-03 01:17:09 +0000

    Implement `excludeCompile` properly
    - Changefd the logic for exclude pattern normalisation so that excludeCompile works.. and so that it is a bit closer to gitignore

--- discovery/documents.go
+++ discovery/documents.go
@@ -46,26 +46,52 @@ func normaliseExcludes(patterns []string) []string {
 			p = p[1:]
 		}
 
-		// directory
+		// detect bare globs
+		// i.e. just "test", doesn't contain or end with /, and no glob characters
+		isBare := !strings.ContainsAny(p, "/*?[]")
+
+		// exploit directory, "test/"
 		if strings.HasSuffix(p, "/") {
 			p = strings.TrimSuffix(p, "/")
 			if rootAnchored {
-				p = p + "/**"
+				//p = p + "/**"
+				out = append(out, p+"/**")
 			} else {
-				p = "**/" + p + "/**"
+				//p = "**/" + p + "/**"
+				out = append(out, "**/"+p+"/**")
+			}
+		} else if isBare {
+			// implicit directory, i.e. just "test"
+			if rootAnchored {
+				// append both, because when it is bare, we don't know whether
+				// it's a file or a directory
+				out = append(out, p)
+				out = append(out, p+"/**")
+			} else {
+				out = append(out, "**/"+p)
+				out = append(out, "**/"+p+"/**")
 			}
 		} else {
 			// file or glob
-			if !rootAnchored {
-				p = "**/" + p
+			if rootAnchored {
+				out = append(out, p)
+			} else {
+				out = append(out, "**/"+p)
 			}
 		}
 
-		if negated {
-			p = "!" + p
-		}
+		//if negated {
+		//	p = "!" + p
+		//}
 
-		out = append(out, p)
+		if negated {
+			pos := len(out) - 1
+			out[pos] = "!" + out[pos]
+
+			if isBare {
+				out[pos-1] = "!" + out[pos-1]
+			}
+		}
 	}
 
 	return out
@@ -102,6 +128,18 @@ func isExcluded(rel string, excludes []string, includes []string) bool {
 	return false
 }
 
+// DiscoverDocuments returns a list of all HTML and static files in the given root directory
+//
+// During discovery, excludes is a list of gitignore-style glob patterns
+// which define what files must be entirely ignored during the discovery process.
+// This means that any matching file is entirely excluded from discovery
+// (and any subsequent processes which rely on it, such as document compilation).
+// As a result, they will not be copied to the output directory at all.
+//
+// excludeCompile, on the other hand, is also a list of gitignore-style glob patterns.
+// However, instead of entire exclusion, it simply reclassifies files as static
+// so that future stages such as compilation will not attempt to compile them,
+// but they still get copied to the output directory.
 func DiscoverDocuments(root string, excludes []string, excludeCompile []string) (*DocumentLists, error) {
 	lists := &DocumentLists{}
 
@@ -109,6 +147,9 @@ func DiscoverDocuments(root string, excludes []string, excludeCompile []string) 
 	excludes = normaliseExcludes(excludes)
 	//fmt.Println(excludes)
 	excludePatterns, includePatterns := splitPatterns(excludes)
+
+	excludeCompile = normaliseExcludes(excludeCompile)
+	excludeCompilePatterns, includeCompilePatterns := splitPatterns(excludeCompile)
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -139,7 +180,7 @@ func DiscoverDocuments(root string, excludes []string, excludeCompile []string) 
 		}
 
 		ext := filepath.Ext(strings.ToLower(info.Name()))
-		if (ext == ".htm" || ext == ".html" || ext == ".shtml" || ext == ".xhtml") && !isExcluded(relPath, excludeCompile, includePatterns) {
+		if (ext == ".htm" || ext == ".html" || ext == ".shtml" || ext == ".xhtml") && !isExcluded(relPath, excludeCompilePatterns, includeCompilePatterns) {
 			lists.HtmlFiles = append(lists.HtmlFiles, path)
 		} else {
 			lists.StaticFiles = append(lists.StaticFiles, path)
